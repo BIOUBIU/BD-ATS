dc639f145288b085d2fcbab970967e6de45083c8
BIOUBIU <64895945+BIOUBIU@users.noreply.github.com>
2023-07-17 12:41:18 +0800

    Update CTRL.py

--- RaspberryPi/CTRL.py
+++ RaspberryPi/CTRL.py
@@ -3,6 +3,9 @@
 import serial
 import time
 import RPi.GPIO as GPIO
+import os
+import WBFM
+import NBFM
 
 serCmd = serial.Serial("/dev/ttyAMA1", 9600, timeout = 10)
 serUSB = serial.Serial("/dev/ttyUSB0", 9600, timeout = 10)#######################
@@ -13,51 +16,63 @@ bufList = ['0']
 def setupSerial():
     global serCmd#, serUSB
     while not serCmd.is_open():
-        pass
-'''    
+        pass  
     while not serUSB.is_open():
         pass
     return
-'''
+
 ##############################
-def setupGPredict():
+def setupPredict():
     return
 
-def taskArrange(satName, mode, freq, time):
+def taskArrange(satName, mode, freq, startTime, endTime):
     return
 
 def tleStorage(satName, tleLn1, tleLn2):
     return
 
+def timeCorrection(time):
+    cmd = "date " + time
+    os.system(cmd)
+    return
+
 def info():#######################
     return
 
-def superdo():
+def superdo(command):
+    os.system(command)
     return
 
 """
     @描述:通过REP语句向北斗上位机请求任务语句
 """
-def REPTaskRequest():
-    global serCmd, buf, bufList #, serUSB
+def REPRequest():
+    global serCmd, buf, bufList, serUSB
+    serCmd.write(b"REP,timeRequest\n")
+    if(serCmd.in_waiting() != 0):
+        buf = serCmd.read_until()
+        bufList = buf.split(',')
+        if(bufList[0] == 'REP'):
+            timeCorrection(bufList[1])
+    ##############################################
     serCmd.write(b"REP,taskRequest\n")
     if(serCmd.in_waiting() != 0):
         buf = serCmd.read_until()
         bufList = buf.split(',')
         if(bufList[0] == 'TSK'):
-            taskArrange(bufList[1], bufList[2], bufList[3], bufList[4])
+            taskArrange(bufList[1], bufList[2], bufList[3], bufList[4], bufList[5])#1：名称 2：调制 3：边带 4：中心频点（Hz） 5：唤醒
     return
 
 def destory():
     serCmd.close()
-    #serUSB.close()
+    serUSB.close()
     GPIO.cleanup()
     return
 
 if __name__ == '__main__':
     try:
         setupSerial()
-        #REPTaskRequest()
+        REPRequest()
         while True:
             if (serCmd.in_waiting() != 0):
                 buf = serCmd.read_until()
@@ -66,10 +81,10 @@ if __name__ == '__main__':
                     taskArrange(bufList[1], bufList[2], bufList[3], bufList[4])
                 elif(bufList[0] == 'TLE'):
                     tleStorage(bufList[1], bufList[2], bufList[3])
-                elif(bufList[0] == 'INF'):
-                    info()
+                #elif(bufList[0] == 'INF'):
+                #    info()
                 elif(bufList[0] == 'SUP'):
-                    superdo()
+                    superdo(bufList[1])
                 else:
                     raise
 
